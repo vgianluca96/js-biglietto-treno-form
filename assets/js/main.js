96db8e39af475c424c61ad944a1c5d2e870cca2b
vgianluca96 <gianluca.vallese@hotmail.com>
2023-08-24 15:13:23 +0200

    milestone 1
    creata logica calcolo prezzo in javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,46 @@
 
 
-console.log('ciao');
+// Dicharazione variabili
+let userName;
+let distanceKm;
+let discount;
+
+// Prezzo al km
+const unitPrice = 0.21;
+
+// function di calcolo del prezzo del biglietto
+function ticketPrice () {
+
+    // Salvataggio variabili
+    userName = document.getElementById('InputUserName').value;
+    distanceKm = document.getElementById('InputdistanceKm').value;
+    discount = document.getElementById('userAge').value;
+
+
+    // Prezzo biglietto
+    price = unitPrice * distanceKm;
+
+    /*
+    console.log(userName);
+    console.log(distanceKm);
+    console.log(discount);
+    */
+    
+    // Applicazione sconto
+    if (discount == 'discount20') {
+        price = price * 0.8;
+    } else if (discount == 'discount40') {
+        price = price * 0.6;
+    }
+
+    // Arrotondamento prezzo
+    price = price.toFixed(2);
+
+    // console.log(price);
+
+    document.getElementById('outputPrice').innerHTML = price;
+    
+    
+}
+
+
